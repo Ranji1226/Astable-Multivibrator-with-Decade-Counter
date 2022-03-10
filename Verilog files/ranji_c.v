b420325c141f70ec2c18a083a294cd7d4d27868d
Ranjith S <81255347+Ranji1226@users.noreply.github.com>
2022-03-10 23:41:57 +0530

    Add files via upload

---
+++ Verilog files/ranji_c.v
@@ -0,0 +1,13 @@
+module ranji_c(q,reset,clk);
+input clk,reset;
+output reg [3:0]q;
+wire a;
+assign a=q[1]&&q[3];
+always@(posedge reset or negedge clk or posedge a)
+begin
+if(reset || a)
+q<=4'b0;
+else
+q<=q+1;
+end
+endmodule
